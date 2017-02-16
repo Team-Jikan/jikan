90c9f6d93db6bedafd15fab2943db29778b651fe
Kevin Jones <joneskj55@gmail.com>
2017-02-16 11:24:40 -0600

    Spring boot initializer.

---
+++ src/main/java/com/jikan/JikanApplication.java
@@ -0,0 +1,12 @@
+package com.jikan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JikanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JikanApplication.class, args);
+	}
+}
