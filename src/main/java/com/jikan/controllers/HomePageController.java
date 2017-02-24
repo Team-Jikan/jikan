4c97abe1b57d0ca753f125ac60e89b49d4f668e7
V-Rod <pjvelazquez@me.com>
2017-02-24 12:23:30 -0600

    - Created HomePageController - refactored home.html code to reflect thymeleaf conventions

---
+++ src/main/java/com/jikan/controllers/HomePageController.java
@@ -0,0 +1,16 @@
+package com.jikan.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by V-Rod on 2/24/17.
+ */
+@Controller
+public class HomePageController {
+
+    @GetMapping("/")
+    public String homePage(){
+        return "home";
+    }
+}
