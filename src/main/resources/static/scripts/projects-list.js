2d3f13ee4b47939daf5cbd57263190084b11036f
V-Rod <pjvelazquez@me.com>
2017-02-25 15:56:06 -0600

    Update projects-list.js

--- src/main/resources/static/scripts/projects-list.js
+++ src/main/resources/static/scripts/projects-list.js
@@ -1,18 +1,18 @@
 (function () {
     var request = $.ajax( {
-        url: '/posts.json'
+        url: '/projects/index.json'
     });
 
-    request.done(function (posts) { //the HTTP response -> an array of JSON objects
-        console.log(posts);
+    request.done(function (projects) { //the HTTP response -> an array of JSON objects
+        //console.log(projects);
 
         var i, html = '';
 
-        for (i = 0; i < posts.length; i++) {
-            html += '<div><h2>' + posts[i].title + '</h2><p>' + posts[i].description + '</p></div>';
+        for (i = 0; i < projects.length; i++) {
+            html += '<div><h2>' + projects[i].title + '</h2><p>' + projects[i].description + '</p></div>';
         }
 
-        $('#load-posts').html(html);
+        $('#load-projects').html(html);
 
     });
 })();
