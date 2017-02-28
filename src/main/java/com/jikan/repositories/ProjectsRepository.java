a9cc8478682cfba847a5ac8596f0c39c6cb76ce3
V-Rod <pjvelazquez@me.com>
2017-02-28 17:45:40 -0600

    Listing projects by user when they log in -ProjectsController.java: code changed to findByUser(userService.loggedInUser().getId()  - ProjectsRepository.java: created query to select project for the given user id

--- src/main/java/com/jikan/repositories/ProjectsRepository.java
+++ src/main/java/com/jikan/repositories/ProjectsRepository.java
@@ -1,8 +1,11 @@
 package com.jikan.repositories;
 
 import com.jikan.models.Project;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by V-Rod on 2/23/17.
@@ -11,6 +14,9 @@ import org.springframework.stereotype.Repository;
 public interface ProjectsRepository extends CrudRepository<Project, Integer>{
 
     public Project findById(Integer id);
+
+    @Query("select p from Project p where p.user.id=?1")
+    public List<Project> findByUser(int userId);
 
 
 }
