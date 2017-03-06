762d64b10637cfb909217ad4fca0c84aae6ff008
V-Rod <pjvelazquez@me.com>
2017-03-06 10:10:03 -0600

    Changed code from main method for deployment
    -JikanApplication.java; pom.xml - added code for deployment
    - home.html: removed splash page from title

--- src/main/java/com/jikan/JikanApplication.java
+++ src/main/java/com/jikan/JikanApplication.java
@@ -2,11 +2,18 @@ package com.jikan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JikanApplication {
+public class JikanApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JikanApplication.class, args);
 	}
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JikanApplication.class);
+    }
 }
